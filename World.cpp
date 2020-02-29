2136feaab37d09648eacb0fc1111bc8b193ec68e
Saul Juarez <saulzini@gmail.com>
2020-02-29 11:41:10 -0600

    improving messages

--- World.cpp
+++ World.cpp
@@ -13,8 +13,8 @@ using namespace std;
 World::World()
 {
 	//Creating the rooms
-	Room *room1 = new Room("Room 1", "You are in the Room 1, in the room there is a desk and on it there is a letter, in addition to a door in the east side.");
-	Room *room2 = new Room("Room 2", "You are in the Room 2, in the room there is a desk and on it there is a note, in addition there is one door to the east and one to the south.");
+	Room *room1 = new Room("Room 1", "You are in the Room 1, in the room there is a letter, in addition to a door in the east side.");
+	Room *room2 = new Room("Room 2", "You are in the Room 2, in the room there is a note, in addition there is one door to the east and one to the south.");
 	Room *room3 = new Room("Room 3", "You are in the Room 3, in the room there is a bag to carry more items, in addition to a door in the south.");
 	Room *room4 = new Room("Room 4", "You are in the Room 4, in the room there is a key and a sword.");
 	MonsterRoom *room5 = new MonsterRoom("Room 5", "You are in the Room 5, You have found the monster that guards the exit, Prepare to fight with your sword.","It looks there is a door to the north");
@@ -53,7 +53,7 @@ World::World()
 	exitTwoToFive->SetLocked(true);
 
 	//Creating items 
-	Item *letterRoom1 = new Item("letter", "In your path you will find two doors, one will lead you to an endless path and the other is protected by a monster",room1, ItemType::LETTER);
+	Item *letterRoom1 = new Item("letter", "In your path you will find two doors, one will lead you to a path where there is not a way out and the other is protected by a monster",room1, ItemType::LETTER);
 	Item *letterRoom2 = new Item("note", "Joseph: I could not carry a weapon because I am missing a bag, I will try my luck, I hope to not die in here",room2, ItemType::NOTE);
 	Item *bag = new Item("bag", "Bag for collecting the items", room3, ItemType::BAG);
 	Item* key = new Item("key", "Key for openning a door", room4, ItemType::KEY);
